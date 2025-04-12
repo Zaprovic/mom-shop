49205c0e8a9c28a251658f6a95f952166f435e37
Zaprovic <jazaro2001@gmail.com>
2025-04-12 00:39:28 -0500

    chore(page-account): small change to translate text

--- src/app/(site)/account/page.tsx
+++ src/app/(site)/account/page.tsx
@@ -20,7 +20,7 @@ const AccountPage = async () => {
   return (
     <div className="mx-auto max-w-7xl px-4 pt-6 sm:px-6 lg:px-8">
       <div className="mb-4 flex flex-col gap-4 sm:mb-8 sm:flex-row sm:items-center sm:justify-between">
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<div>Cargando...</div>}>
           <User />
         </Suspense>
         <div className="flex flex-col gap-4 sm:flex-row">
