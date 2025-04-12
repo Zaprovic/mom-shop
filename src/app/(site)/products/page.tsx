fc8011d685a995d9044dd01d20194717f50ab7cf
Zaprovic <jazaro2001@gmail.com>
2025-04-12 00:01:35 -0500

    chore(product-home): small change on the product home page

--- src/app/(site)/products/page.tsx
+++ src/app/(site)/products/page.tsx
@@ -1,4 +1,10 @@
-const ProductsPage = () => {
+export const revalidate = 0;
+
+type props = {
+  searchParams: { categoryId: string; productName: string };
+};
+
+const ProductsPage = async ({ searchParams }: props) => {
   return <div>ProductsPage</div>;
 };
 
