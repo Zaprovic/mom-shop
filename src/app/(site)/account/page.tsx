932fe7ee2099bc6f12a36ce26fb75b2f3a447871
Zaprovic <jazaro2001@gmail.com>
2025-04-11 23:48:45 -0500

    feat(product-page): added prop to product creation form to receive categories
    This is due to the fact that the product creation form is a client component, thus it cannot handle
    async operations. To get around this, now it receives the categories as a prop, and these categories
    are fetched asynchronously in the product page

--- src/app/(site)/account/page.tsx
+++ src/app/(site)/account/page.tsx
@@ -5,35 +5,60 @@ import { Suspense } from "react";
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import CreateProductForm from "./_components/create-product-form";
+import CreateProductForm from "./_components/product-form";
+import CategoryCreationForm from "./_components/category-form";
+import { db } from "@/db";
+import { categoriesTable } from "@/db/schema";
 
 const AccountPage = async () => {
+  const categories = await db.select().from(categoriesTable);
   return (
     <div className="mx-auto max-w-7xl px-4 pt-6 sm:px-6 lg:px-8">
       <div className="mb-4 flex flex-col gap-4 sm:mb-8 sm:flex-row sm:items-center sm:justify-between">
         <Suspense fallback={<div>Loading...</div>}>
           <User />
         </Suspense>
-        <Dialog modal>
-          <DialogTrigger asChild>
-            <Button className="w-full sm:w-auto">
-              <PlusCircleIcon className="mr-2 h-4 w-4" />
-              <span className="-tracking-wider whitespace-nowrap">
-                Agregar nuevo producto
-              </span>
-            </Button>
-          </DialogTrigger>
-          <DialogContent >
-            <DialogHeader>
-              <DialogTitle>Agregar nuevo producto</DialogTitle>
-            </DialogHeader>
-            <CreateProductForm />
-          </DialogContent>
-        </Dialog>
+        <div className="flex flex-col gap-4 sm:flex-row">
+          <Dialog modal>
+            <DialogTrigger asChild>
+              <Button className="w-full sm:w-auto" size={"sm"}>
+                <PlusCircleIcon className="mr-2 h-4 w-4" />
+                <span className="-tracking-wider whitespace-nowrap">
+                  Agregar nuevo producto
+                </span>
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Productos</DialogTitle>
+              </DialogHeader>
+              <DialogDescription className="hidden"></DialogDescription>
+              <CreateProductForm categories={categories} />
+            </DialogContent>
+          </Dialog>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button className="w-full sm:w-auto" size={"sm"}>
+                <PlusCircleIcon className="mr-2 h-4 w-4" />
+                <span className="-tracking-wider whitespace-nowrap">
+                  Agregar nueva categoria
+                </span>
+              </Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Categorias</DialogTitle>
+              </DialogHeader>
+              <CategoryCreationForm />
+              <DialogDescription className="hidden"></DialogDescription>
+            </DialogContent>
+          </Dialog>
+        </div>
       </div>
     </div>
   );
