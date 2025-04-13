c8790b004701945731c2b782df00a313ba045b46
Zaprovic <jazaro2001@gmail.com>
2025-04-12 22:10:13 -0500

    feat: added stats and fetching for products and categories
    A stats function was provided as a prop that calculates aggregates for the count of categories, and
    the products are fetched asynchronously

--- src/app/(site)/account/page.tsx
+++ src/app/(site)/account/page.tsx
@@ -13,10 +13,43 @@ import {
 import CreateProductForm from "./_components/product-form";
 import CategoryCreationForm from "./_components/category-form";
 import { db } from "@/db";
-import { categoriesTable } from "@/db/schema";
+import { productsTable, categoriesTable } from "@/db/schema";
+import { count, eq } from "drizzle-orm";
+import { Button } from "@/components/ui/button";
+import Cards from "./_components/cards";
+import { RefreshCwIcon } from "lucide-react";
+import { DataTable } from "./_components/product-form/_components/data-table";
+import { columns } from "./_components/product-form/_components/data-table/columns";
+
+async function getStats(userId: string) {
+  const [productCount] = await db
+    .select({ count: count() })
+    .from(productsTable)
+    .where(eq(productsTable.userId, userId));
+
+  const [categoryCount] = await db
+    .select({ count: count() })
+    .from(categoriesTable)
+    .where(eq(categoriesTable.userId, userId));
+
+  return {
+    productCount: productCount.count,
+    categoryCount: categoryCount.count,
+  };
+}
 
 const AccountPage = async () => {
-  const categories = await db.select().from(categoriesTable);
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const userId = user?.id || "";
+  const stats = await getStats(userId);
+
+  const products = await db
+    .select()
+    .from(productsTable)
+    .where(eq(productsTable.userId, userId));
+
   return (
     <div className="mx-auto max-w-7xl px-4 pt-6 sm:px-6 lg:px-8">
       <div className="mb-4 flex flex-col gap-4 sm:mb-8 sm:flex-row sm:items-center sm:justify-between">
