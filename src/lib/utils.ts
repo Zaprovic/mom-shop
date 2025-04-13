a6af57211ea2927945eb274fa0d5b727f0d7cefd
Zaprovic <jazaro2001@gmail.com>
2025-04-12 21:21:20 -0500

    feat: added util function to handle price format conversion

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx";
+import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function formatCurrency(amount: number) {
+  const roundedAmount = Math.ceil(amount / 100) * 100;
+
+  const price = new Intl.NumberFormat("es-CO", {
+    style: "currency",
+    currency: "COP",
+    minimumFractionDigits: 0,
+  }).format(roundedAmount);
+
+  return price;
 }
