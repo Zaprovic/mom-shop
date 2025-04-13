5cd7c412354fe0206536e017acca644d02ade1e3
Zaprovic <jazaro2001@gmail.com>
2025-04-13 17:16:24 -0500

    fix: synchronized cart number with global store

--- src/components/globals/navbar-icons.tsx
+++ src/components/globals/navbar-icons.tsx
@@ -5,8 +5,10 @@ import { Button } from "../ui/button";
 import Link from "next/link";
 import { ModeToggle } from "./mode-toggle";
 import { LoginLink } from "@kinde-oss/kinde-auth-nextjs";
+import { useCartStore } from "@/stores/cart-store";
 
 const NavbarIcons = () => {
+  const { items } = useCartStore();
   return (
     <div className="flex items-center space-x-1 sm:space-x-2">
       <Button
@@ -37,9 +39,15 @@ const NavbarIcons = () => {
       >
         <Link href="/cart" aria-label="Go to cart" title="Go to cart">
           <ShoppingCartIcon className="h-4 w-4" />
-          <span className="absolute -right-1 -top-1 flex h-4 w-4 items-center justify-center rounded-full bg-primary text-[10px] font-medium text-primary-foreground">
-            0
-          </span>
+          {items.length > 0 ? (
+            <span className="bg-primary text-primary-foreground absolute top-0 right-0 flex aspect-square h-auto w-4 items-center justify-center rounded-full text-[0.625rem]">
+              {items.length}
+            </span>
+          ) : (
+            <span className="bg-primary text-primary-foreground absolute top-0 right-0 flex aspect-square h-auto w-4 items-center justify-center rounded-full text-[0.625rem]">
+              0
+            </span>
+          )}
         </Link>
       </Button>
 
